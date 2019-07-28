9839606930a5362d94dd36affc6c6373de374f8f
Steven Balerna <stevenbalerna@stevens-air.attlocal.net>
2019-07-27 20:25:19 -0400

    js file updates

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ("bacon cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("plain hamburger");
